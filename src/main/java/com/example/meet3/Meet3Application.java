7c8974bf32fdfe43ddca8c36d0f07fc5b9de5293
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-03-11 23:57:31 +0700

    Initial commit

---
+++ src/main/java/com/example/meet3/Meet3Application.java
@@ -0,0 +1,13 @@
+package com.example.meet3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meet3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meet3/Meet3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meet3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meet3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
